96b801c870c82a064cbf566394b345c94dd6b9e7
raelactao <rvlpics@gmail.com>
2024-12-09 22:58:22 +0800

    Revert "payment statuses updated"
    This reverts commit 2e4bbff010499cde34bc20c674fda2466661b35c.

--- regainmobile/src/main/java/com/isproj2/regainmobile/repo/PaymentRepository.java
+++ regainmobile/src/main/java/com/isproj2/regainmobile/repo/PaymentRepository.java
@@ -1,12 +1,9 @@
 package com.isproj2.regainmobile.repo;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.isproj2.regainmobile.model.Payment;
-import com.isproj2.regainmobile.model.User;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
